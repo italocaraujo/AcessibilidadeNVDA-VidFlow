9d3b431d6c739a43421ec3c71e6ccd1c08e442c6
Italo Correia <128881501+italocaraujo@users.noreply.github.com>
2024-09-21 05:14:19 -0300

    Add files via upload

--- AcessibilidadeVidFlow/js/botoesCategorias.js
+++ AcessibilidadeVidFlow/js/botoesCategorias.js
@@ -4,10 +4,20 @@ botoesCategorias.forEach((botao) => {
   botao.addEventListener("click", () => {
     const categoriaSelecionada = botao.getAttribute("name");
 
+    associarPainel(categoriaSelecionada);
     filtrarPorCategoria(categoriaSelecionada);
     atualizarEstadosDosBotoes(categoriaSelecionada);
   });
+
+  botao.addEventListener("keydown", mudarFocoPorTeclado);
 });
+
+function associarPainel(categoriaSelecionada) {
+  const painelVideos = document.querySelector('[role="tabpanel"]');
+  const idBotao = document.querySelector(`[name="${categoriaSelecionada}"]`).id;
+
+  painelVideos.setAttribute("aria-labelledby", idBotao);
+}
 
 function filtrarPorCategoria(filtro) {
   const videos = document.querySelectorAll(".videos__item");
@@ -26,6 +36,27 @@ function atualizarEstadosDosBotoes(categoriaSelecionada) {
   botoesCategorias.forEach((botao) => {
     const botaoFoiSelecionado = botao.getAttribute("name") === categoriaSelecionada;
 
-    botao.classList.toggle("selecionado", botaoFoiSelecionado);
+    botao.ariaSelected = botaoFoiSelecionado;
+    botao.setAttribute("tabindex", botaoFoiSelecionado ? 0 : -1);
   })
+}
+
+const tablist = document.querySelector('[role="tablist"]');
+
+function mudarFocoPorTeclado(evento) {
+  const botaoAtual = evento.target;
+
+  if (evento.key === "ArrowRight") {
+    if (botaoAtual === tablist.lastElementChild) {
+      tablist.firstElementChild.focus();
+    } else {
+      botaoAtual.nextElementSibling.focus();
+    }
+  } else if (evento.key === "ArrowLeft") {
+    if (botaoAtual === tablist.firstElementChild) {
+      tablist.lastElementChild.focus();
+    } else {
+      botaoAtual.previousElementSibling.focus();
+    }
+  }
 }
